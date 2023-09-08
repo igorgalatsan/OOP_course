04745bffd63901344eab02fbd920890994b75168
igorgalatsan <igorgalatsan@gmai.com>
2023-09-08 09:49:23 +0300

    test completed

--- static_class_method10.py
+++ static_class_method10.py
@@ -1,27 +1,54 @@
-d = {"app":False,'app2':False,'app3':True}
-d.pop('app')
-#print(len(d.keys()))
-#print(d)
+'''
+Объявите класс AppStore - интернет-магазин приложений для устройств под iOS.
+ В этом классе должны быть реализованы следующие методы:
 
+add_application(self, app) - добавление нового приложения app в магазин;
+remove_application(self, app) - удаление приложения app из магазина;
+block_application(self, app) - блокировка приложения app (устанавливает локальное
+ свойство blocked объекта app в значение True);
+total_apps(self) - возвращает общее число приложений в магазине.
+
+Класс AppStore предполагается использовать следующим образом (эти строчки в 
+программе не писать):
+
+store = AppStore()
+app_youtube = Application("Youtube")
+store.add_application(app_youtube)
+store.remove_application(app_youtube)
+
+Здесь Application - класс, описывающий добавляемое приложение с указанным именем.
+ Каждый объект класса Application должен содержать локальные свойства:
+
+name - наименование приложения (строка);
+blocked - булево значение (True - приложение заблокировано; False - не 
+заблокировано, изначально False).
+
+Как хранить список приложений в объектах класса AppStore решите сами.
+'''
 class AppStore:
     DICT_APP = {}
     
     def add_application(self, app):
-        self.DICT_APP.update({app.name:app.blocked })
+        self.DICT_APP[id(app)]= app
        
     def remove_application(self, app):
-        self.DICT_APP.pop(app.name)
+        self.DICT_APP.pop(id(app))
 
     def block_application(self, app):
-        self.DICT_APP[app.name]=True
+        obj = self.DICT_APP.get(id(app),False)
+        if not obj:
+            return False
+        obj.blocked = True
+        return True 
+        
     def total_apps(self) :
-        return len(self.DICT_APP.keys())
+        return len(self.DICT_APP)
 
 
 class Application:
-    def __init__(self,name,blocked=False) -> None:
+    def __init__(self,name) -> None:
         self.name = name
-        self.blocked = blocked
+        self.blocked = False
 
 store = AppStore()
 app_youtube = Application("Youtube")
